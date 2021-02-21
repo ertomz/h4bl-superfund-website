2366ddd02652afc39699c8b52733bbb5b5459f41
chloe <chloesun15@gmail.com>
2021-02-21 15:34:01 -0800

    update email

--- superfund.js
+++ superfund.js
@@ -10,24 +10,38 @@ function parseStateCity(data, state, city) {
     let senatorContact1 = data[state]["senators"]["contact_site_1"];
     let senator2 = data[state]["senators"]["senator_name_2"];
     let senatorContact2 = data[state]["senators"]["contact_site_2"];
-    let stateCount = data[state]["superfund_sites"];
+    let stateCount = parseInt(data[state]["superfund_sites"]);
     let cityCount = 0;
     let highPop = false;
+    let censusPercent = 0;
 
     if (city in data[state]["city"]){
         cityCount = data[state]["city"][city]["superfund_count"];
-        if ("black_pop_g13" in data[state]["city"][city]){
-            highPop = true;
-        }
-        console.log(cityCount);
     }
 
-    senatorString = `Your senators are: <a href=${senatorContact1}>${senator1}</a> and <a href=${senatorContact2}>${senator2}</a>`
-    cityString = cityCount < 0 ? "" : cityCount > 1 ?  `There are ${cityCount} superfunds in my city, ${city}.` : `There is ${cityCount} superfund in my city, ${city}.`;
-    stateString = `There are ${stateCount} superfunds in the state of ${state}`;
+    if ("sf_black_g13" in data[state]){
+        console.log(data[state]);
+        console.log(data[state]["sf_black_g13"]);
+        censusCount = parseInt(data[state]["sf_black_g13"]);
+        console.log(censusCount);
+        console.log(stateCount);
+        censusPercent = ((censusCount/stateCount)*100).toFixed(2);
 
+    }
+
+    senatorString = `Your senators are: <a href=${senatorContact1}>${senator1}</a> and <a href=${senatorContact2}>${senator2}</a>`;
+
+    
+    header = `To The Honorable ${senator1}:`
+    introduction = cityCount > 0 ? `I am writing from ${city}, ${state} in regards to our ${stateCount} Superfund sites in ${state}.`
+                                 : `I am writing from ${city}, ${state} in regards to our ${cityCount} Superfund sites in ${city} and ${stateCount} in ${state}.`    
+    
+    percentage = censusPercent > 0 ? `Of those in ${state}, ${censusPercent}% are in areas with higher than average proportion of Black Americans in their population.`
+                                   : ""
+
+    
     document.getElementById("senators").innerHTML = senatorString;
-    document.getElementById("replace").innerHTML = `Dear ${senator1}... ${cityString}...${stateString}, ${highPop}`
+    document.getElementById("replace").innerHTML = header + introduction + percentage;
 
 }
 
@@ -48,7 +62,7 @@ function onEmailClick(){
     data_loaded = false;
 
     if (data_loaded == false){
-        req.open("GET", "https://api.jsonbin.io/b/6031d4d1d677700867e5c65b/8", true);
+        req.open("GET", "https://api.jsonbin.io/b/6031d4d1d677700867e5c65b/9", true);
         req.setRequestHeader("secret-key", superfundKey);
         req.send();
 
--- superfund.py
+++ superfund.py
@@ -24,25 +24,6 @@ def state_data(data, senators, census):
 
     state_stats = dict()
 
-        for i in range(len(senators)):
-        senator = senators[i][15] 
-        state_name = senators[i][0]
-        state = [i][2]
-        contact = senators[i][46]
-
-        if state not in state_stats.keys():
-            state_stats[state_code] = dict()
-
-        if "senators" not in state_stats[state].keys():
-            state_stats[state]["senators"] = dict()
-            state_stats[state]["senators"]["senator_name_1"] = senator
-            state_stats[state]["senators"]["contact_site_1"] = contact
-            state_stats[state]["state_name"] = state_name
-        else:
-            state_stats[state]["senators"]["senator_name_2"] = senator
-            state_stats[state]["senators"]["contact_site_2"] = contact
-            state_stats[state]["state_name"] = state_name
-
     for i in range(len(data)):
         state = data[i][state_index] 
         city = data[i][city_index].replace(" County", "").strip(" ")
@@ -52,20 +33,50 @@ def state_data(data, senators, census):
             state_stats[state]["superfund_sites"] += 1
 
         if city not in state_stats[state]["city"].keys():
-            state_stats[state]["city"][city] = {"superfund_count": 1}
+            state_stats[state]["city"][city] = {"superfund_count": 1, "black_pop_g13": False}
         else:
             state_stats[state]["city"][city]["superfund_count"] += 1
     
+    for i in range(len(senators)):
+        senator = senators[i][15] 
+        state = senators[i][0]
+        contact = senators[i][46]
+
+        if state not in state_stats.keys():
+            state_stats[state] = dict()
+
+        if "senators" not in state_stats[state].keys():
+            state_stats[state]["senators"] = dict()
+            state_stats[state]["senators"]["senator_name_1"] = senator
+            state_stats[state]["senators"]["contact_site_1"] = contact
+        else:
+            state_stats[state]["senators"]["senator_name_2"] = senator
+            state_stats[state]["senators"]["contact_site_2"] = contact
+            
     for i in range(len(census)):
         state = census[i][0]
         city  = census[i][1]
 
         if state in state_stats.keys():
             if city in state_stats[state]["city"].keys():
+                '''
+                if "sf_black_g13" not in state_stats[state].keys():
+                    state_stats[state]["sf_black_g13"] = 1
+                else:
+                    print(city, state)
+                    state_stats[state]["sf_black_g13"] += 1
+                '''
                 state_stats[state]["city"][city]["black_pop_g13"] = True
 
+    for state in state_stats:
+        count = 0
+        for city in state_stats[state]["city"]:
+            if city["black_pop_g13"] == True:
+                count += city["superfund_count"]
 
-    print(state_stats["Georgia"])
+        state["sf_black_g13"] = count
+
+    print(state_stats["Delaware"])
 
     save_to_json(state_stats, "state_data")
 
