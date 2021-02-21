290590097b21f41fc4cd39c8cb0ced67161d2704
chloe <chloesun15@gmail.com>
2021-02-21 10:53:03 -0800

    add city functionality

--- email-stuff/superfund.js
+++ email-stuff/superfund.js
@@ -1,30 +1,53 @@
 const key = "$2b$10$uL8ywCwlTQxBy85XQ6pHe.nOtVeD2otuBBCudT0kar6DrS/IgZ4Fm";
 
+function parseStateCity(data, state, city) {
+    let senator1 = data[state]["senators"]["senator_name_1"];
+    let senatorContact1 = data[state]["senators"]["contact_site_1"];
+    let senator2 = data[state]["senators"]["senator_name_2"];
+    let senatorContact2 = data[state]["senators"]["contact_site_2"];
+    let stateCount = data[state]["superfund_sites"];
+    let cityCount = 0;
+
+    if (city in data[state]["city"]){
+        cityCount = data[state]["city"][city];
+        console.log(cityCount);
+    }
+
+    senatorString = `Your senators are: <a href=${senatorContact1}>${senator1}</a> and <a href=${senatorContact2}>${senator2}</a>`
+    cityString = cityCount < 0 ? "" : cityCount > 1 ?  `There are ${cityCount} superfunds in my city, ${city}.` : `There is ${cityCount} superfund in my city, ${city}.`;
+    stateString = `There are ${stateCount} superfunds in the state of ${state}`;
+
+    document.getElementById("senators").innerHTML = senatorString;
+    document.getElementById("replace").innerHTML = `Dear ${senator1}... ${cityString}...${stateString}`
+
+}
+
 function onClick(){
-    let state = document.getElementById("input").value;
+    let state = document.getElementById("input-state").value;
+    let city = document.getElementById("input-city").value;
 
     let req = new XMLHttpRequest();
 
     req.onreadystatechange = () => {
         if (req.readyState == XMLHttpRequest.DONE) {
             data = JSON.parse(req.responseText);
-            senator1 = data[state]["senator_name_1"];
-            senatorContact1 = data[state]["contact_site_1"];
-            senator2 = data[state]["senator_name_2"];
-            senatorContact2 = data[state]["contact_site_2"];
 
-            string = `Your senators are: <a href=${senatorContact1}>${senator1}</a> and <a href=${senatorContact2}>${senator2}</a>`
-
-            console.log(senatorContact1);
-
-            document.getElementById("senators").innerHTML = string;
-            document.getElementById("replace").innerHTML = `Dear ${senator1}... `
+            console.log(data[state]);
+            
+            parseStateCity(data, state, city);
 
         };}
 
-    req.open("GET", "https://api.jsonbin.io/b/6031d4d1d677700867e5c65b", true);
-    req.setRequestHeader("secret-key", key);
-    req.send();
+    data_loaded = false;
+
+    if (data_loaded == false){
+        req.open("GET", "https://api.jsonbin.io/b/6031d4d1d677700867e5c65b/3", true);
+        req.setRequestHeader("secret-key", key);
+        req.send();
+
+    } else {
+        parseStateCity(data, state, city);
+    }
 
 }
 
--- email-stuff/superfund.py
+++ email-stuff/superfund.py
@@ -24,15 +24,22 @@ def state_data(data, senators):
     senator_state_index = 0
     senator_name_index = 15
     senator_contact_index = 46
+    city_index = 6
 
     state_stats = dict()
 
     for i in range(len(data)):
         state = data[i][state_index] 
+        city = data[i][city_index].replace(" County", "").strip(" ")
         if state not in state_stats.keys():
-            state_stats[state] = {"superfund_sites": 1}
+            state_stats[state] = {"superfund_sites": 1, "city": dict()}
         else:
             state_stats[state]["superfund_sites"] += 1
+
+        if city not in state_stats[state]["city"].keys():
+            state_stats[state]["city"][city] = 1
+        else:
+            state_stats[state]["city"][city] += 1
 
     for i in range(len(senators)):
         senator = senators[i][senator_name_index] 
@@ -42,20 +49,21 @@ def state_data(data, senators):
         if state not in state_stats.keys():
             state_stats[state] = dict()
 
-        if "senator_name_1" not in state_stats[state].keys():
-            state_stats[state]["senator_name_1"] = senator
-            state_stats[state]["contact_site_1"] = contact
+        if "senators" not in state_stats[state].keys():
+            state_stats[state]["senators"] = dict()
+            state_stats[state]["senators"]["senator_name_1"] = senator
+            state_stats[state]["senators"]["contact_site_1"] = contact
         else:
-            state_stats[state]["senator_name_2"] = senator
-            state_stats[state]["contact_site_2"] = contact
+            state_stats[state]["senators"]["senator_name_2"] = senator
+            state_stats[state]["senators"]["contact_site_2"] = contact
     
     print(state_stats["Georgia"])
 
     save_to_json(state_stats, "state_data")
 
 def main():
-    data = get_data("superfund.csv")
-    senators = get_data("us-senate.csv")
+    data = get_data("./superfund.csv")
+    senators = get_data("./us-senate.csv")
 
     state_data(data, senators)
     
